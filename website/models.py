a0a094066409e58538c3dd8b9b0270cf1ad789e3
Axel <93802093+jeoxn@users.noreply.github.com>
2025-12-05 09:48:17 +0700

    [UPDATE] Migrating database to mysql

--- website/models.py
+++ website/models.py
@@ -6,8 +6,8 @@ from datetime import datetime
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    kunci_publik = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    kunci_publik = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.password}')"
